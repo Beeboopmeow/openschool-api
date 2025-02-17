37313c627b1cd541c675d70a41c4cd4a042e58a9
Constanze <beeboopmeow@gmail.com>
2025-02-17 17:09:16 -0300

    adicionar DTO de requisição de endereço

--- src/main/java/com/openschool/api/model/dtos/request/AddressRequestDTO.java
+++ src/main/java/com/openschool/api/model/dtos/request/AddressRequestDTO.java
@@ -1,4 +1,33 @@
 package com.openschool.api.model.dtos.request;
 
-public class AddressRequestDTO {
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AddressRequestDTO(
+        @NotBlank(message = "código postal é obrigatório")
+        @Pattern(regexp = "\\d{5}-\\d{3}|\\d{8}", message = "Código postal inválido")
+        @Schema(description = "Código postal", example = "12345-678 ou 12345678")
+        String zipCode,
+
+        @NotBlank(message = "estado é obrigatório")
+        @Schema(description = "Estado", example = "Bahia")
+        String state,
+
+        @NotBlank(message = "cidade é obrigatória")
+        @Schema(description = "Cidade", example = "São Paulo")
+        String city,
+
+        @NotBlank(message = "bairro é obrigatório")
+        @Schema(description = "Bairro", example = "Centro")
+        String neighborhood,
+
+        @Schema(description = "Rua", example = "Rua das Flores")
+        String street,
+
+        @Schema(description = "Número residencial", example = "352")
+        String number
+) {
+
 }
